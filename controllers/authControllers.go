2b3e787280ecd2cce2ff307961bf6e223496e6e8
refoilmiya <refo.ilmiya@gmail.com>
2019-12-05 17:20:15 +0700

    initial commit

---
+++ controllers/authControllers.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"tkai_circles_account/models"
+	u "tkai_circles_account/utils"
+)
+
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp, err := account.Create() //Create account
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		u.Respond(w, u.Message(false, err.Error()))
+		return
+	}
+	u.Respond(w, resp)
+}
+
+var Authenticate = func(w http.ResponseWriter, r *http.Request) {
+
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp, err := models.Login(account.Username, account.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		u.Respond(w, u.Message(false, err.Error()))
+		return
+	}
+	u.Respond(w, resp)
+}
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+	"tkai_circles_account/app"
+	"tkai_circles_account/controllers"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	//router.NotFoundHandler = app.NotFoundHandler
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
